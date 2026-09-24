732d8a3554c991d44fbf616b9f9e3dae81718d03
Egor Dianov <gordian99@yandex.ru>
2026-09-24 21:27:07 +0300

    Задачи седьмого этапа

--- catalog_analysis.py
+++ catalog_analysis.py
@@ -197,3 +197,33 @@ above_average = {
 }
 
 print(above_average)
+
+
+
+def all_genres(movies):
+    genres = set()
+
+    for movie in movies:
+        genres.update(movie["genres"])
+
+    return genres
+    
+print(sorted(all_genres(movies)))
+
+
+def common_actors(movie1, movie2):
+    actors1 = set(movie1["actors"])
+    actors2 = set(movie2["actors"])
+
+    return actors1 & actors2
+
+print(common_actors(movies[0], movies[3]))
+
+
+def genres_only_in_one(movies_a, movies_b):
+    genres_a = all_genres(movies_a)
+    genres_b = all_genres(movies_b)
+
+    return genres_a - genres_b
+    
+print(genres_only_in_one(movies[5:6], movies[:5]))
